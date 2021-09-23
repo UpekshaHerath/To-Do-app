e525981005392d1d3832b48f690a430bfdc4c511
UpekshaHerath <upekshadilshan000@gmail.com>
2021-09-23 01:06:43 -0700

    made the GUI

--- functions.js
+++ functions.js
@@ -1,1 +1,6 @@
-console.log('name is Upe');
+let textArea = document.getElementById('floatingTextarea');
+let add_button = document.getElementById('add_button');
+
+add_button.onclick = function() {
+    console.log(`The inputs : ${textArea.value}`);
+}
