6e70cca7193afa475f5e300d3ca25ab7bd862945
UpekshaHerath <upekshadilshan000@gmail.com>
2021-10-16 17:33:04 -0700

    Added a new property

--- functions.js
+++ functions.js
@@ -1,6 +1,18 @@
 let textArea = document.getElementById('floatingTextarea');
 let add_button = document.getElementById('add_button');
+let todo = document.getElementById('listItem');
+let todo_label = document.getElementById("to-do-label");
 
 add_button.onclick = function() {
-    console.log(`The inputs : ${textArea.value}`);
+    todo_listCreate();
 }
+
+function todo_listCreate() {
+    //create a paragraph element to add as the todo item
+    let node = document.createElement("p");
+    let textnode = document.createTextNode(textArea.value); //asign the text for textnode variable
+    node.appendChild(textnode); //append the child textnode to node object
+    document.getElementById("todo_list").appendChild(node);
+    textArea.value = "";
+}
+
